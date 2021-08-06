c4f94ab87b8b63a3e01de7a8e334b02403d5ae3a
Md Majharul Islam <80042446+majharul-web@users.noreply.github.com>
2021-08-06 14:18:58 +0600

    largest and smallest number calculet

---
+++ problem-solving/smallest-number.js
@@ -0,0 +1,14 @@
+function findLargestNumber(num1, num2, num3) {
+    if (num1 < num2 && num1 < num3) {
+        return num1;
+    }
+    else if (num2 < num1 && num2 < num3) {
+        return num2;
+    }
+    else {
+        return num3
+    }
+
+}
+var smallNumber = findLargestNumber(8, 12, 4);
+console.log(smallNumber);
